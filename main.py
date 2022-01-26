c70d81dc4ad0429145302fa5c60e9346508af6ab
Jared Johnston <jazzab@gmail.com>
2022-01-26 17:03:13 +1000

    Add players

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS game (
   id INTEGER PRIMARY KEY AUTOINCREMENT
   ,status TEXT
-  ,num_players INTEGER
+)
+        """)
+cursor.execute("""
+
+CREATE TABLE IF NOT EXISTS player (
+  game_id INTEGER
+  ,player_name TEXT
 )
         """)
 
@@ -21,7 +27,7 @@ connection.close()
 async def root():
     connection = sqlite3.connect('game.db')
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO game (status, num_players) VALUES ('not started', 0)")
+    cursor.execute("INSERT INTO game (status) VALUES ('not started')")
     cursor.execute("SELECT MAX(id) FROM game")
     row = cursor.fetchall()
     new_game_id = row[0][0]
@@ -30,14 +36,34 @@ async def root():
     return {"message": f"you just created a new game. Your game id is {new_game_id}."} # FIXME: return the link to the new game.
 
 @app.get("/{game_id}")
-async def get_status(game_id):
+async def get_status(game_id: int):
     connection = sqlite3.connect('game.db')
     cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM game WHERE id = {game_id}")
-    row = cursor.fetchall()
-    game_status = row[0][1]
-    num_players = row[0][2]
+    cursor.execute(f"SELECT status FROM game WHERE id = {game_id}")
+    row = cursor.fetchone()
+    game_status = row[0]
+    cursor.execute(f"SELECT player_name FROM player WHERE game_id = {game_id}")
+    rows = cursor.fetchall()
+    player_names = [i[0] for i in rows]
+    num_players = len(player_names)
     connection.commit()
     connection.close()
-    return {"message": f"get status of game id = {game_id}", "game status": f"{game_status}", "number of players": f"{num_players}"}
+    return {"message": f"get status of game id = {game_id}", "game status": f"{game_status}", "number of players": f"{num_players}", "players": f"{sorted(player_names)}"}
+
+@app.get("/{game_id}/{player_name}")
+async def player_page(game_id: int, player_name: str):
+    connection = sqlite3.connect('game.db')
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT player_name FROM player WHERE game_id = '{game_id}' AND player_name = '{player_name}'")
+    row = cursor.fetchone()
+    if row == None:
+        cursor.execute(f"INSERT INTO player (game_id, player_name) VALUES ('{game_id}', '{player_name}')")
+        message = {"message": f"welcome {player_name}, you have been added to the game."}
+    elif len(row) == 1:
+        message = {"message": f"welcome back {player_name}"}
+    else:
+        message = {"message": f"error, there are two players named '{player_name}' in game_id = {game_id}"}
+    connection.commit()
+    connection.close()
+    return message
 
--- readme.md
+++ readme.md
@@ -1,14 +1,11 @@
 Avalon
 ======
 
-This is to automate the first part of the game Avalon so that the game can be set up without cards, and rule books, and speeches, etc.
+This is to automate the game Avalon so that the game can be played without cards, and rule books, and speeches, etc.
 
 How to play
 -----------
 
-`python avalon.py`
+`uvicorn main:app --reload`
 
-Debugging
----------
-
-Debug messages are sent to avalon.log. Run `tailf avalon.log` in one tab and then `python avalon.py` in another tab if you are having problems.
+Then visit the url that is printed.
